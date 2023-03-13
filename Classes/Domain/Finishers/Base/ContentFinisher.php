601ca5ae41374aa14e060684d3ce3e753c937bc0
Manuel Schnabel <mschnabel1992@gmail.com>
2023-03-13 10:10:06 +0100

    [BUGFIX] Added check for type before replacing dynamic values

--- Classes/Domain/Finishers/Base/ContentFinisher.php
+++ Classes/Domain/Finishers/Base/ContentFinisher.php
@@ -60,7 +60,9 @@ abstract class ContentFinisher extends AbstractFinisher
     protected function replaceFormVariablesWithUserInputs($content) {
         $formValues = $this->finisherContext->getFormRuntime()->getFormState()->getFormValues();
         foreach($formValues as $key => $value) {
-            $content = str_replace("{".$key."}", $value, $content);
+            if (gettype($value) === "string") {
+                $content = str_replace("{".$key."}", $value, $content);
+            }
         }
         return $content;
     }
