32df8fad959864089e40cc219ec15e0b37a59e68
Manuel Schnabel <service@passionweb.de>
2024-11-14 21:26:41 +0100

    [FIX] add fallbacks for option params and extend error handling

--- Classes/Domain/Finishers/ExtendFluidEmailFinisher.php
+++ Classes/Domain/Finishers/ExtendFluidEmailFinisher.php
@@ -3,6 +3,7 @@
 namespace Passionweb\FormEmailContentblocks\Domain\Finishers;
 
 
+use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Resource\Exception\InvalidFileException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
@@ -12,44 +13,41 @@ class ExtendFluidEmailFinisher extends AbstractFinisher
 {
     protected function executeInternal()
     {
-        /**
-         * check if a valid hexidecimal string was entered
-         */
-        if(ctype_xdigit(substr($this->options['bgColor'], 1)) && (strlen($this->options['bgColor']) === 4 || strlen($this->options['bgColor']) === 7)) {
+        $bgColor = $this->options['bgColor'] ?? '';
+        if(ctype_xdigit(substr($bgColor, 1)) && (strlen($bgColor) === 4 || strlen($bgColor) === 7)) {
             $this->finisherContext->getFinisherVariableProvider()->add(
                 $this->shortFinisherIdentifier,
                 'backgroundColor',
                 $this->options['bgColor']
             );
         }
-
-        /**
-         * get absolute path based on entered logo path
-         */
-        $absoluteLogoPath = $this->generateAbsolutePathOfFile($this->options['logo']);
-
-        $this->finisherContext->getFinisherVariableProvider()->add(
-            $this->shortFinisherIdentifier,
-            'logo',
-            $absoluteLogoPath
-        );
+        $logoPath = $this->options['logo'] ?? '';
+        $absoluteLogoPath = $this->generateAbsolutePathOfFile($logoPath);
+        if(empty($absoluteLogoPath)) {
+            $this->finisherContext->getFinisherVariableProvider()->add(
+                $this->shortFinisherIdentifier,
+                'logo',
+                $absoluteLogoPath
+            );
+        }
     }
 
     /**
      * @param string $logoPath
      * @return string
-     * @throws InvalidFileException
      */
     private function generateAbsolutePathOfFile(string $logoPath) {
-        // entered file path is an extension path
-        if(PathUtility::isExtensionPath($logoPath)) {
-            return GeneralUtility::locationHeaderUrl(PathUtility::getPublicResourceWebPath($logoPath));
-        }
-        // entered file path is a relative fileadmin path
-        else {
-            // get base uri
-            $baseUri = $this->finisherContext->getFormRuntime()->getRequest()->getAttribute('normalizedParams')->getSiteUrl();
-            return $baseUri . "/". ltrim($logoPath, '/');
+        try {
+            if(PathUtility::isExtensionPath($logoPath)) {
+                return GeneralUtility::locationHeaderUrl(PathUtility::getPublicResourceWebPath($logoPath));
+            } else {
+                $baseUri = $this->finisherContext->getFormRuntime()->getRequest()->getAttribute('normalizedParams')->getSiteUrl();
+                return $baseUri . "/". ltrim($logoPath, '/');
+            }
+        } catch (InvalidFileException $e) {
+            $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
+            $logger->error($e->getMessage());
+            return '';
         }
     }
 }
