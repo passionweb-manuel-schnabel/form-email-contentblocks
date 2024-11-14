81e8976a4c7f09c839160b0649075ea290bf27a5
Manuel Schnabel <service@passionweb.de>
2024-11-14 20:28:45 +0100

    [TASK] integrate typoscript via addTypoScript

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,9 @@
 <?php
-defined('TYPO3') || die();
 
-(static function() {})();
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    'form_email_contentblocks',
+    'setup',
+    '@import "EXT:form_email_contentblocks/Configuration/TypoScript/setup.typoscript"'
+);
